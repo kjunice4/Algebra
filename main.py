42f0e419c3e66b2d730c9c9b37c50e3109bfd39a
Kevin Junice <37306150+kjunice4@users.noreply.github.com>
2022-04-16 10:36:27 -0400

    Update main.py

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ Builder.load_string("""
             background_color: 0, 0 , 0 , 1
             size_hint_y: None
             height: 200
-            text: "KSquared-math,LLC © : Algebra Bundle"
+            text: "KSquared-Mathematics : Algebra Bundle"
             on_release:
                 app.root.current = "Menu"
                 root.manager.transition.direction = "left" 
@@ -147,16 +147,26 @@ Builder.load_string("""
                 on_release:
                     app.root.current = "updates"
                     root.manager.transition.direction = "left"
-                    
+            
+            Button:
+                font_size: 75
+                size_hint_y: None
+                height: 200
+                padding: 10, 10
+                text: "Visit KSquared-Mathematics"
+                on_release:
+                    import webbrowser
+                    webbrowser.open('https://www.ksquaredmathematics.com/subscribe')        
+            
             Label:
                 font_size: 75
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
-                text: "Share KSquared-math,LLC ©"
+                text: "Share KSquared-Mathematics"
                     
             Image:
-                source: 'KSquared_QR_code.png'
+                source: 'KSquared_QR.png'
                 size_hint_y: None
                 height: 1000
                 width: 1000
@@ -188,7 +198,7 @@ Builder.load_string("""
                 size_hint_y: None
                 height: 200
                 padding: 10, 10
-                text: "What's new at KSquared-math?"
+                text: "What's new at KSquared-Mathematics?"
             
             Button:
                 id: steps
